d4c3b2cd21182cc4dc8eba8cc7696e773e645604
FranciscoBelando <1514980@alu.murciaeduca.es>
2025-02-05 21:35:50 +0100

    Punto 7
    Subida de archivos: .gitignore,Principal.java y readme.md

---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        FuncionCompletamenteUtilYFuncionalParaImprimirPorPantallaHolaMundo();
+    }
+
+    public static void FuncionCompletamenteUtilYFuncionalParaImprimirPorPantallaHolaMundo() {
+        System.out.println("\"Hola mundo\"");
+    }
+}
